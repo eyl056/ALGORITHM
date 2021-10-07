129dac6dd922f502572695fc4b279bf4896d53b7
enyung <45346786+eyl056@users.noreply.github.com>
2021-10-08 00:14:59 +0900

    연습문제

---
+++ PROGRAMMERS/LEVEL1/최대공약수와 최소공배수.py
@@ -0,0 +1,12 @@
+def gcd(n, m):
+    while m:
+        n, m = m, n % m
+    return n
+
+def lcm(n, m):
+    return n * m / gcd(n, m)
+
+def solution(n, m):
+    answer = []
+    answer.extend([gcd(n, m), lcm(n, m)])
+    return answer
